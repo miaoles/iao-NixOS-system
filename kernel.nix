91d2d6c623591ba0506b6124e0380da2f0437397
miaoles <iao_mm@pm.me>
2021-10-13 03:17:54 -0400

    corrections

--- kernel.nix
+++
@@ -1,6 +0,0 @@
-{ config, pkgs, ... }:
-
-{
-	# Use the xanmod cacule kernel.
-	boot.kernelPackages = pkgs.linuxPackages_xanmod;
-}
--- security.nix
+++
@@ -1,5 +0,0 @@
-{ config, pkgs, ... }:
-
-{
-	security.apparmor.enable = true;
-}
